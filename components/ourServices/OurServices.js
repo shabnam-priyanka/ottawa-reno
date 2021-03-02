067910dea57c84c8fdb7286ca44c702d34ca54d0
shabnam-priyanka <shabnam.priyanka@live.ca>
2021-03-02 02:27:54 -0500

    shabz

--- components/ourServices/OurServices.js
+++ components/ourServices/OurServices.js
@@ -5,17 +5,45 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 const images = [
     {
-      original: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg',
-      thumbnail: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg',
+      original: 'https://i.imgur.com/s0zsG7h.jpg',
+      thumbnail: 'https://i.imgur.com/s0zsG7h.jpg',
     },
     {
-      original: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg',
-      thumbnail: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg'
+      original: 'https://i.imgur.com/0XrjB42.jpg',
+      thumbnail: 'https://i.imgur.com/0XrjB42.jpg'
     },
     {
-      original: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg',
-      thumbnail: 'https://cdn.pixabay.com/photo/2019/05/23/21/56/heath-the-bumble-bee-4224954__340.jpg'
-    }
+      original: 'https://i.imgur.com/iL5bsid.jpg',
+      thumbnail: 'https://i.imgur.com/iL5bsid.jpg'
+    },
+    {
+        original: 'https://i.imgur.com/YMQNLSr.jpg',
+        thumbnail: 'https://i.imgur.com/YMQNLSr.jpg',
+      },
+      {
+        original: 'https://i.imgur.com/MZ2rjsp.jpg',
+        thumbnail: 'https://i.imgur.com/MZ2rjsp.jpg'
+      },
+      {
+        original: 'https://i.imgur.com/xRalC1J.jpg',
+        thumbnail: 'https://i.imgur.com/xRalC1J.jpg'
+      },
+      {
+        original: 'https://i.imgur.com/TOChYKq.jpg',
+        thumbnail: 'https://i.imgur.com/TOChYKq.jpg',
+      },
+      {
+        original: 'https://i.imgur.com/ASOvx5l.jpg',
+        thumbnail: 'https://i.imgur.com/ASOvx5l.jpg'
+      },
+      {
+        original: 'https://i.imgur.com/CSVvsaz.jpg',
+        thumbnail: 'https://i.imgur.com/CSVvsaz.jpg'
+      },
+      {
+        original: 'https://i.imgur.com/2iSE2B3.jpg',
+        thumbnail: 'https://i.imgur.com/2iSE2B3.jpg'
+      }
   ]
 
 const OurServices = () => {
