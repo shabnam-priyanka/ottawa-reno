3bc8ec5be9385ad9462c8a1cb5cb606176e7d800
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-02 10:16:04 -0800

    done @Mahbub-Hasan1

--- components/bookOnline/BookOnline.js
+++ components/bookOnline/BookOnline.js
@@ -5,22 +5,19 @@ const book = [
         id: 0,
         name: 'Kitchen Renovation',
         details: 'We are specialized in modern kitchen design',
-        img:
-            'https://i.imgur.com/kJvTuYb.jpg',
+        img: 'https://i.imgur.com/kJvTuYb.jpg',
     },
     {
         id: 1,
         name: 'Living Room Renovation',
         details: 'Quality of our work will fulfill your dream living area',
-        img:
-            'https://i.imgur.com/hDHv43p.jpg',
+        img: 'https://i.imgur.com/hDHv43p.jpg',
     },
     {
         id: 2,
         name: 'Bedroom Renovation',
         details: 'We will delivery the best Modern bedroom',
-        img:
-            'https://i.imgur.com/tkn4HoF.jpg',
+        img: 'https://i.imgur.com/tkn4HoF.jpg',
     },
     {
         id: 3,
@@ -31,34 +28,32 @@ const book = [
     },
 ];
 
-const BookOnline = () => { 
-
-    const bookingList = book.map(booking => {
+const BookOnline = () => {
+    const bookingList = book.map((booking) => {
         return (
             <div className="card">
-            <div className="card-image">
-                <img src={booking.img} alt="img" />
-                <span className="card-title">{booking.title}</span>
-            </div>
-            <div className="card-content">
-                <p>{booking.details}</p>
-                <div className="card-action">
-                    <Link href="/">
-                        <a>Book for Consultation</a>
-                    </Link>
+                <div className="card-image">
+                    <img src={booking.img} alt="img" />
+                    <span className="card-title">{booking.title}</span>
+                </div>
+                <div className="card-content">
+                    <p>{booking.details}</p>
+                    <div className="card-action">
+                        <Link href="/">
+                            <a>Book for Consultation</a>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
-        )
-      })
+        );
+    });
 
-return (
-    <div>
-        <h1>Shabnam code here</h1>
-        <div className="rootCard">
-        {bookingList}
-      </div>
-    </div>
-)};
+    return (
+        <div>
+            <h1>Shabnam code here</h1>
+            <div className="rootCard">{bookingList}</div>
+        </div>
+    );
+};
 
 export default BookOnline;
