fb10f0d2f9fd505edafffe334d212adc9cf0014e
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-01 16:40:02 -0800

    @Mahbub-Hasan

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+
   return (
     <div className={styles.Home}>
-      <h1>This is home</h1>
+        <h1>This is home</h1>
     </div>
   );
 }
