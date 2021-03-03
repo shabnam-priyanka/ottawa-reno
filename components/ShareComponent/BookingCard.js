a64917469a705eacb168b4c87b3c29b862ae74ab
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-03 01:05:39 -0800

    dkd

---
+++ components/ShareComponent/BookingCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BookingCard = () => {
+    return (
+        <div className="booking-card">
+            <h4>Kitchen Renovations</h4>
+            <h5>1 hr</h5>
+            <hr />
+            <p>March 22, 2021 10:00 am</p>
+            <p>Marc Perron</p>
+            <button>Next</button>
+        </div>
+    );
+};
+
+export default BookingCard;
