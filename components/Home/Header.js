6d7edf3c38fb57c7ec5ba1965724d45f55d9fa87
shabnam-priyanka <shabnam.priyanka@live.ca>
2021-03-05 21:44:31 -0500

    Shabnam

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -4,15 +4,15 @@ import Navbar from '../ShareComponent/Navbar';
 const Header = () => {
     const hello = 5;
     return (
-        <div className="header-area">
+        <div className="header-area ">
             <Navbar background="rgba(255, 255, 255, 0)" />
             <div className="header-content-area  text-right">
             <header className="container-xl">
                 <div className="row">
                     <div className="col-md-4 p-0"></div>
                     <div className="col-md-8">
-                        <h1 className="header-main-content">Ottawa-Reno</h1>
-                        <h5 className="header-main-title">We serve the best quality of product</h5>
+                        <h1 className="header-main-content"><strong>Ottawa-Reno</strong> </h1>
+                        <h5 className="header-main-title"> <strong>Best quality & trust!</strong> </h5>
                         {/* <img
                             className="img-fluid"
                             src="https://static.wixstatic.com/media/2af8e1_0facbe1190cf4810929163b23a239f1d~mv2.png/v1/fill/w_518,h_413,al_c,q_85,usm_0.66_1.00_0.01/FullColor-TransparentBg-1280x1024-72dpi_.webp"
