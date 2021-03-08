af0d7a0babd9de7506a227530c409c21aec028d0
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-08 11:01:42 -0800

    done

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -11,8 +11,8 @@ const Header = () => {
                 <div className="row">
                     <div className="col-md-3 p-0"></div>
                     <div className="col-md-9">
-                        <h1 className="header-main-content text-truncate"><strong>Ottawa-Reno</strong> </h1>
-                        <h5 className="header-main-title"> <strong>Best quality & trust!</strong> </h5>
+                        <h1 className="header-main-content text-truncate">Ottawa-Reno </h1>
+                        <h5 className="header-main-title"> Best quality & trust! </h5>
                         {/* <img
                             className="img-fluid"
                             src="https://static.wixstatic.com/media/2af8e1_0facbe1190cf4810929163b23a239f1d~mv2.png/v1/fill/w_518,h_413,al_c,q_85,usm_0.66_1.00_0.01/FullColor-TransparentBg-1280x1024-72dpi_.webp"
